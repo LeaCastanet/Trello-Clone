973868b54ebb3e5461f2b94cd16b6b2f72d1ffa4
Léa Castanet <lea@MacBook-Pro-de-Lea.local>
2023-01-10 10:25:18 +0100

    ajout toggle

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,25 @@
 "use strict";
 const itemsContainer = document.querySelectorAll(".items-container");
+let actualContainer;
+let actualBtn;
+let actualUL;
+let actualForm;
+let actualTextInput;
+let actualValidation;
 function addContainerListeners(currentContainer) {
     const currentContainerDeletionBtn = currentContainer.querySelector(".delete-container-btn");
+    const currentAddItemBtn = currentContainer.querySelector(".add-item-btn");
     deleteBtnListeners(currentContainerDeletionBtn);
+    addItemBtnListeners(currentAddItemBtn);
 }
 itemsContainer.forEach((container) => {
     addContainerListeners(container);
 });
 function deleteBtnListeners(btn) {
     btn.addEventListener("click", handleContainerDeletion);
+}
+function addItemBtnListeners(btn) {
+    btn.addEventListener("click", handleAddItem);
 }
 function handleContainerDeletion(e) {
     const btn = e.target;
@@ -19,4 +30,30 @@ function handleContainerDeletion(e) {
         ...document.querySelectorAll(".items-container"),
     ];
     containers[btnsArray.indexOf(btn)].remove();
+}
+function handleAddItem(e) {
+    const btn = e.target;
+    if (actualContainer) {
+        toggleForm(actualBtn, actualForm, false);
+    }
+    setContainerItems(btn);
+    toggleForm(actualBtn, actualForm, true);
+}
+function toggleForm(btn, form, action) {
+    if (!action) {
+        form.style.display = "none";
+        btn.style.display = "block";
+    }
+    else if (action) {
+        form.style.display = "block";
+        btn.style.display = "none";
+    }
+}
+function setContainerItems(btn) {
+    actualBtn = btn;
+    actualContainer = btn.parentElement;
+    actualUL = actualContainer.querySelector("ul");
+    actualForm = actualContainer.querySelector("form");
+    actualTextInput = actualContainer.querySelector("input");
+    actualValidation = actualContainer.querySelector(".validation-msg");
 }
